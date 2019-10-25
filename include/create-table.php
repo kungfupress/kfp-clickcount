62acffaee49dca48eb63d0671400449f060cc592
Juanan Ruiz <juananruizrivas@gmail.coml.com>
2019-10-25 21:03:46 +0200

    Agrega campo para fecha último click

--- include/create-table.php
+++ include/create-table.php
@@ -13,17 +13,18 @@ defined( 'ABSPATH' ) || die();
  */
 function kfp_clickcount_create_table() {
 	global $wpdb;
-	$sql              = array();
-	$clickcount_table = $wpdb->prefix . 'kfp_clickcount';
-	$charset_collate  = $wpdb->get_charset_collate();
+	$sql             = array();
+	$table           = $wpdb->prefix . 'kfp_clickcount';
+	$charset_collate = $wpdb->get_charset_collate();
 	// Consulta para crear las tablas
 	// Mas adelante utiliza dbDelta, si la tabla ya existe no la crea sino que la
 	// modifica con los posibles cambios y sin pérdida de datos.
-	$sql[] = "CREATE TABLE $clickcount_table (
+	$sql[] = "CREATE TABLE $table (
 		id mediumint(9) NOT NULL AUTO_INCREMENT,
 		link varchar(500) NOT NULL UNIQUE,
 		clicks bigint(20),
-		date_first_click date,
+		date_first_click datetime,
+		date_last_click datetime,
 		PRIMARY KEY (id)
 		) $charset_collate";
 	include_once ABSPATH . 'wp-admin/includes/upgrade.php';
--- kfp-click-count.php
+++ kfp-click-count.php
@@ -1,31 +1,51 @@
 <?php
 /**
- * Plugin Name: KFP Click Count
+ * Plugin Name:   KFP Click Count
  * Plugin Author: KungFuPress
- * Description: Cuenta los clicks en enlaces o botones de tu web
+ * Description:   Cuenta los clicks en enlaces o botones de tu web
+ * Plugin URI:    https://github.com/kungfupress/kfp-clickcount
+ * Version:       0.1.0
+ * Author:        Juanan Ruiz
+ * Author URI:    https://kungfupress.com/
+ *
+ * @package KFP ClickCount
  */
 
-defined( 'ABSPATH' ) or die();
-$ruta_plugin = plugin_dir_path(__FILE__);
+defined( 'ABSPATH' ) || die();
+$ruta_plugin = plugin_dir_path( __FILE__ );
 
-// Crea la tabla para los enlaces al activar el plugin
-include_once($ruta_plugin . "include/create-table.php");
-register_activation_hook(__FILE__, 'kfp_clickcount_create_table');
+// Crea la tabla para los enlaces al activar el plugin.
+require_once $ruta_plugin . 'include/create-table.php';
+register_activation_hook( __FILE__, 'kfp_clickcount_create_table' );
 
-// Agrega el script que estará vigilando nuestra web
+// Agrega el script que estará vigilando nuestra web.
 add_action( 'wp_enqueue_scripts', 'kfp_clickcount_enqueue_scripts' );
-function kfp_clickcount_enqueue_scripts()
-{
-  wp_enqueue_script('kfp-clickcount', plugins_url('js/clickcount.js', __FILE__), array('jquery'), '1.0.0', true );
-  wp_localize_script( 'kfp-clickcount', 'AjaxParams', array(
-    'adminAjaxUrl' => admin_url('admin-ajax.php'),
-    'nonce' => wp_create_nonce('clickcount-nonce')
-    ));
+/**
+ * Agrega el fichero javascript a la cola
+ *
+ * @return void
+ */
+function kfp_clickcount_enqueue_scripts() {
+	wp_enqueue_script(
+		'kfp-clickcount',
+		plugins_url( 'js/clickcount.js', __FILE__ ),
+		array( 'jquery' ),
+		'1.0.0',
+		true
+	);
+	wp_localize_script(
+		'kfp-clickcount',
+		'AjaxParams',
+		array(
+			'adminAjaxUrl' => admin_url( 'admin-ajax.php' ),
+			'nonce'        => wp_create_nonce( 'clickcount-nonce' ),
+		)
+	);
 }
 
-// Procesa la pulsación de un enlace mediante AJAX
-include_once($ruta_plugin . "include/procesa-click.php");
+// Procesa la pulsación de un enlace mediante AJAX.
+require_once $ruta_plugin . 'include/procesa-click.php';
 
-// Shortcodes para mostrar la lista de clicks en escritorio o en la web
-include_once($ruta_plugin . "include/admin-click-list.php");
-include_once($ruta_plugin . "include/public-click-list.php");
+// Shortcodes para mostrar la lista de clicks en escritorio o en la web.
+require_once $ruta_plugin . 'include/admin-click-list.php';
+require_once $ruta_plugin . 'include/public-click-list.php';
